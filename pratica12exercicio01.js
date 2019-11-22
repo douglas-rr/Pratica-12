2712f8dc5ee31d459d0dd60e337f483c99f0aabc
Douglas Ramos <55302738+douglas-rr@users.noreply.github.com>
2019-11-22 01:38:27 -0300

    Add files via upload

---
+++ pratica12exercicio01.js
@@ -0,0 +1,263 @@
+function mostrarCoringa () {
+    var
+    divCartaz = document.getElementById ("cartazCoringa").style.display = "none",
+    divTexto = document.getElementById ("textoCoringa").style.display = "none";
+
+    if (divCartaz == "none" && divTexto == "none") {
+        document.getElementById ("cartazCoringa").style.display = "block";
+        document.getElementById ("textoCoringa").style.display = "block";
+    }
+}
+
+function fecharCoringa () {
+    var
+    divCartaz2 = document.getElementById ("cartazCoringa").style.display = "block",
+    divTexto2 = document.getElementById ("textoCoringa").style.display = "block";
+
+    if (divCartaz2 == "block" && divTexto2 == "block"){
+        document.getElementById ("cartazCoringa").style.display = "none";
+        document.getElementById ("textoCoringa").style.display = "none";
+    }
+}
+
+function mostrarConvergente () {
+    var
+    divCartaz = document.getElementById ("cartazConvergente").style.display = "none",
+    divTexto = document.getElementById ("textoConvergente").style.display = "none";
+
+    if (divCartaz == "none" && divTexto == "none") {
+        document.getElementById ("cartazConvergente").style.display = "block";
+        document.getElementById ("textoConvergente").style.display = "block";
+    }
+}
+
+function fecharConvergente () {
+    var
+    divCartaz2 = document.getElementById ("cartazConvergente").style.display = "block",
+    divTexto2 = document.getElementById ("textoConvergente").style.display = "block";
+
+    if (divCartaz2 == "block" && divTexto2 == "block"){
+        document.getElementById ("cartazConvergente").style.display = "none";
+        document.getElementById ("textoConvergente").style.display = "none";
+    }
+}
+
+function mostrarFamilia () {
+    var
+    divCartaz = document.getElementById ("cartazFamilia").style.display = "none",
+    divTexto = document.getElementById ("textoFamilia").style.display = "none";
+
+    if (divCartaz == "none" && divTexto == "none") {
+        document.getElementById ("cartazFamilia").style.display = "block";
+        document.getElementById ("textoFamilia").style.display = "block";
+    }
+}
+
+function fecharFamilia () {
+    var
+    divCartaz2 = document.getElementById ("cartazFamilia").style.display = "block",
+    divTexto2 = document.getElementById ("textoFamilia").style.display = "block";
+
+    if (divCartaz2 == "block" && divTexto2 == "block"){
+        document.getElementById ("cartazFamilia").style.display = "none";
+        document.getElementById ("textoFamilia").style.display = "none";
+    }
+}
+
+function mostrarHarry () {
+    var
+    divCartaz = document.getElementById ("cartazHarry").style.display = "none",
+    divTexto = document.getElementById ("textoHarry").style.display = "none";
+
+    if (divCartaz == "none" && divTexto == "none") {
+        document.getElementById ("cartazHarry").style.display = "block";
+        document.getElementById ("textoHarry").style.display = "block";
+    }
+}
+
+function fecharHarry () {
+    var
+    divCartaz2 = document.getElementById ("cartazHarry").style.display = "block",
+    divTexto2 = document.getElementById ("textoHarry").style.display = "block";
+
+    if (divCartaz2 == "block" && divTexto2 == "block"){
+        document.getElementById ("cartazHarry").style.display = "none";
+        document.getElementById ("textoHarry").style.display = "none";
+    }
+}
+
+function mostrarMalevola () {
+    var
+    divCartaz = document.getElementById ("cartazMalevola").style.display = "none",
+    divTexto = document.getElementById ("textoMalevola").style.display = "none";
+
+    if (divCartaz == "none" && divTexto == "none") {
+        document.getElementById ("cartazMalevola").style.display = "block";
+        document.getElementById ("textoMalevola").style.display = "block";
+    }
+}
+
+function fecharMalevola () {
+    var
+    divCartaz2 = document.getElementById ("cartazMalevola").style.display = "block",
+    divTexto2 = document.getElementById ("textoMalevola").style.display = "block";
+
+    if (divCartaz2 == "block" && divTexto2 == "block"){
+        document.getElementById ("cartazMalevola").style.display = "none";
+        document.getElementById ("textoMalevola").style.display = "none";
+    }
+}
+
+function mostrarLeao () {
+    var
+    divCartaz = document.getElementById ("cartazLeao").style.display = "none",
+    divTexto = document.getElementById ("textoLeao").style.display = "none";
+
+    if (divCartaz == "none" && divTexto == "none") {
+        document.getElementById ("cartazLeao").style.display = "block";
+        document.getElementById ("textoLeao").style.display = "block";
+    }
+}
+
+function fecharLeao () {
+    var
+    divCartaz2 = document.getElementById ("cartazLeao").style.display = "block",
+    divTexto2 = document.getElementById ("textoLeao").style.display = "block";
+
+    if (divCartaz2 == "block" && divTexto2 == "block"){
+        document.getElementById ("cartazLeao").style.display = "none";
+        document.getElementById ("textoLeao").style.display = "none";
+    }
+}
+
+function mostrarShrek () {
+    var
+    divCartaz = document.getElementById ("cartazShrek").style.display = "none",
+    divTexto = document.getElementById ("textoShrek").style.display = "none";
+
+    if (divCartaz == "none" && divTexto == "none") {
+        document.getElementById ("cartazShrek").style.display = "block";
+        document.getElementById ("textoShrek").style.display = "block";
+    }
+}
+
+function fecharShrek () {
+    var
+    divCartaz2 = document.getElementById ("cartazShrek").style.display = "block",
+    divTexto2 = document.getElementById ("textoShrek").style.display = "block";
+
+    if (divCartaz2 == "block" && divTexto2 == "block"){
+        document.getElementById ("cartazShrek").style.display = "none";
+        document.getElementById ("textoShrek").style.display = "none";
+    }
+}
+
+function mostrarVaiQueCola () {
+    var
+    divCartaz = document.getElementById ("cartazVaiQueCola").style.display = "none",
+    divTexto = document.getElementById ("textoVaiQueCola").style.display = "none";
+
+    if (divCartaz == "none" && divTexto == "none") {
+        document.getElementById ("cartazVaiQueCola").style.display = "block";
+        document.getElementById ("textoVaiQueCola").style.display = "block";
+    }
+}
+
+function fecharVaiQueCola () {
+    var
+    divCartaz2 = document.getElementById ("cartazVaiQueCola").style.display = "block",
+    divTexto2 = document.getElementById ("textoVaiQueCola").style.display = "block";
+
+    if (divCartaz2 == "block" && divTexto2 == "block"){
+        document.getElementById ("cartazVaiQueCola").style.display = "none";
+        document.getElementById ("textoVaiQueCola").style.display = "none";
+    }
+}
+
+function mostrarValente () {
+    var
+    divCartaz = document.getElementById ("cartazValente").style.display = "none",
+    divTexto = document.getElementById ("textoValente").style.display = "none";
+
+    if (divCartaz == "none" && divTexto == "none") {
+        document.getElementById ("cartazValente").style.display = "block";
+        document.getElementById ("textoValente").style.display = "block";
+    }
+}
+
+function fecharValente () {
+    var
+    divCartaz2 = document.getElementById ("cartazValente").style.display = "block",
+    divTexto2 = document.getElementById ("textoValente").style.display = "block";
+
+    if (divCartaz2 == "block" && divTexto2 == "block"){
+        document.getElementById ("cartazValente").style.display = "none";
+        document.getElementById ("textoValente").style.display = "none";
+    }
+}
+
+function mostrarVingadores () {
+    var
+    divCartaz = document.getElementById ("cartazVingadores").style.display = "none",
+    divTexto = document.getElementById ("textoVingadores").style.display = "none";
+
+    if (divCartaz == "none" && divTexto == "none") {
+        document.getElementById ("cartazVingadores").style.display = "block";
+        document.getElementById ("textoVingadores").style.display = "block";
+    }
+}
+
+function fecharVingadores () {
+    var
+    divCartaz2 = document.getElementById ("cartazVingadores").style.display = "block",
+    divTexto2 = document.getElementById ("textoVingadores").style.display = "block";
+
+    if (divCartaz2 == "block" && divTexto2 == "block"){
+        document.getElementById ("cartazVingadores").style.display = "none";
+        document.getElementById ("textoVingadores").style.display = "none";
+    }
+}
+
+function mostrarStarWars () {
+    var
+    divCartaz = document.getElementById ("cartazStarWars").style.display = "none",
+    divTexto = document.getElementById ("textoStarWars").style.display = "none";
+
+    if (divCartaz == "none" && divTexto == "none") {
+        document.getElementById ("cartazStarWars").style.display = "block";
+        document.getElementById ("textoStarWars").style.display = "block";
+    }
+}
+
+function fecharStarWars () {
+    var
+    divCartaz2 = document.getElementById ("cartazStarWars").style.display = "block",
+    divTexto2 = document.getElementById ("textoStarWars").style.display = "block";
+
+    if (divCartaz2 == "block" && divTexto2 == "block"){
+        document.getElementById ("cartazStarWars").style.display = "none";
+        document.getElementById ("textoStarWars").style.display = "none";
+    }
+}
+
+function mostrarAvatar () {
+    var
+    divCartaz = document.getElementById ("cartazAvatar").style.display = "none",
+    divTexto = document.getElementById ("textoAvatar").style.display = "none";
+
+    if (divCartaz == "none" && divTexto == "none") {
+        document.getElementById ("cartazAvatar").style.display = "block";
+        document.getElementById ("textoAvatar").style.display = "block";
+    }
+}
+
+function fecharAvatar () {
+    var
+    divCartaz2 = document.getElementById ("cartazAvatar").style.display = "block",
+    divTexto2 = document.getElementById ("textoAvatar").style.display = "block";
+
+    if (divCartaz2 == "block" && divTexto2 == "block"){
+        document.getElementById ("cartazAvatar").style.display = "none";
+        document.getElementById ("textoAvatar").style.display = "none";
+    }
+}
